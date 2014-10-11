55f6b3f8a3a5a66351974c8f8859d851714fef44
xuthief <xuthief@gmail.com>
2014-10-11 14:12:03 +0800

    done test cases for queue command

--- test/cberl_queue_tests.erl
+++ test/cberl_queue_tests.erl
@@ -6,8 +6,8 @@ cberl_test_() ->
     [{foreach, fun setup/0, fun clean_up/1,
       [
        fun test_lenqueue/1
-       %,fun test_ldequeue/1
-       %,fun test_lremove/1
+       ,fun test_ldequeue/1
+       ,fun test_lremove/1
       ]}].
 
 
@@ -17,9 +17,9 @@ cberl_test_() ->
 
 setup() ->
     cberl:start_link(?POOLNAME, 3),
-    cberl:remove(?POOLNAME, <<"testkey">>),
-    cberl:remove(?POOLNAME, <<"testkey1">>),
-    cberl:remove(?POOLNAME, <<"testkey2">>),
+   %cberl:remove(?POOLNAME, <<"testkey">>),
+   %cberl:remove(?POOLNAME, <<"testkey1">>),
+   %cberl:remove(?POOLNAME, <<"testkey2">>),
     ok.
 
 clean_up(_) ->
@@ -44,33 +44,34 @@ test_lenqueue(_) ->
      ,?_assertMatch({Key, _, [Value, Value, Value2]}, Get3)
     ].
 
-%test_ldequeue(_) ->
-%    Key = <<"testkey">>,
-%    Value = 1,
-%    Key2 = <<"testkey2">>,
-%    Value2 = 2,
-%    DequeueValue = cberl:ldequeue(?POOLNAME, Key, 0),
-%    DequeueFail = cberl:ldequeue(?POOLNAME, Key2, 0),
-%    Get1 = cberl:lget(?POOLNAME, Key),
-%    [
-%     ?_assertMatch({Key, _, [Value, Value2]}, Get1),
-%     ?_assertEqual({error, key_enoent}, DequeueFail),
-%     ?_assertMatch({Key, _, 1}, DequeueValue)
-%    ].
-%
-%test_lremove(_) ->
-%    Key = <<"testkey">>,
-%    Key2 = <<"testkey2">>,
-%    Value = 1,
-%    Value2 = 2,
-%    ok = cberl:lremove(?POOLNAME, Key, 0, Value2),
-%    Get1 = cberl:lget(?POOLNAME, Key),
-%    ok = cberl:lremove(?POOLNAME, Key, 0, Value),
-%    Get2 = cberl:lget(?POOLNAME, Key),
-%    RemoveFail = cberl:lremove(?POOLNAME, Key, 0, Value),
-%    RemoveFail2 = cberl:lremove(?POOLNAME, Key2, 0, Value),
-%    [?_assertMatch({Key, _, [Value]}, Get1),
-%     ?_assertEqual({error, key_enoent}, RemoveFail),
-%     ?_assertEqual({error, key_enoent}, RemoveFail2),
-%     ?_assertEqual({error, key_enoent}, Get2)
-%    ].
+test_ldequeue(_) ->
+    Key = <<"testkey">>,
+    Value = 1,
+    Key2 = <<"testkey2">>,
+    Value2 = 2,
+    DequeueValue = cberl:ldequeue(?POOLNAME, Key, 0),
+    DequeueFail = cberl:ldequeue(?POOLNAME, Key2, 0),
+    Get1 = cberl:lget(?POOLNAME, Key),
+    [
+     ?_assertMatch({Key, _, 1}, DequeueValue)
+     ,?_assertMatch({Key, _, [Value, Value2]}, Get1)
+     ,?_assertEqual({Key2, {error, key_enoent}}, DequeueFail)
+    ].
+
+test_lremove(_) ->
+    Key = <<"testkey">>,
+    Key2 = <<"testkey2">>,
+    Value = 1,
+    Value2 = 2,
+    ok = cberl:lremove(?POOLNAME, Key, 0, Value2),
+    ok = cberl:lremove(?POOLNAME, Key, 0, Value2),
+    Get1 = cberl:lget(?POOLNAME, Key),
+    ok = cberl:lremove(?POOLNAME, Key, 0, Value),
+    Get2 = cberl:lget(?POOLNAME, Key),
+    RemoveFail = cberl:lremove(?POOLNAME, Key, 0, Value),
+    RemoveFail2 = cberl:lremove(?POOLNAME, Key2, 0, Value),
+    [?_assertMatch({Key, _, [Value]}, Get1),
+     ?_assertEqual({error, key_enoent}, RemoveFail),
+     ?_assertEqual({error, key_enoent}, RemoveFail2),
+     ?_assertEqual({Key, {error, key_enoent}}, Get2)
+    ].
